fb418558b8543687ff67858adb7b0c93e48ce19f
Omkar Chavan <83566321+chavan-omkar@users.noreply.github.com>
2026-08-27 18:09:00 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // HashSet<String> seen = new HashSet<>();
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] grid = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int numb = board[i][j] - '1';
+                    int box = (i / 3) * 3 + (j / 3);
+
+                    if(row[i][numb] || col[j][numb] || grid[box][numb]){
+                        return false;
+                    }
+
+                    row[i][numb] = true;
+                    col[j][numb] = true;
+                    grid[box][numb] = true;
+
+                    // if(numb != '.'){
+
+                    //     if(!seen.add(numb + "in_row" + i) ||
+                    //        !seen.add(numb + "in_col" + j) ||
+                    //        !seen.add(numb + "in_box" + (i/3) + "-" + (j/3))){
+                    //         return false;
+                    //        }
+
+                    // }
+                }
+            }
+
+            // return true;
+        }
+        return true;
+    }
+}
