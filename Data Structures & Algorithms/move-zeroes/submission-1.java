f192a04d20fc49720b3eb34a0f751dd933ea939d
Omkar Chavan <omkarchavan1198@gmail.com>
2026-09-23 07:57:48 +0530

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return;
+        }
+        int slow = 0;
+        int fast = 0;
+
+        int n = nums.length;
+
+        while(fast < n){
+            int num = nums[fast];
+
+            if( num != 0 ){
+                nums[fast] = nums[slow];
+                nums[slow] = num;
+                slow++;
+            }
+            fast++;
+        }
+        
+    }
+}
